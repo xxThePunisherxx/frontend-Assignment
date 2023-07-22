14c67a21156881e22257a6a2ebd4e5c1bce113d9
Nikesh Khatri <nikeshkhatri763@gmail.com>
2023-07-22 11:12:27 +0545

    Initilizing routes

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop.js";
+import { ThemeState } from "./CLientSide/context/ThemeState.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { ReactQueryDevtools } from "react-query/devtools";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			{/* <App /> */}
+			<BrowserRouter>
+				<ScrollToTop />
+				<ThemeState>
+					<Routes>
+						<Route path="/*" element={<App />} />
+					</Routes>
+				</ThemeState>
+			</BrowserRouter>
+			<ReactQueryDevtools initialIsOpen />
+		</QueryClientProvider>
+	</React.StrictMode>
+);
